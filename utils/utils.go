7d28b136f2427647b296b91bc0a2baf05c1d2aa2
r0ck3r008 <naman.arora@tutanota.com>
2020-11-10 18:44:20 -0500

    SHA512 hashing functions

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"crypto/sha512"
+	"io/ioutil"
+)
+
+func HashStr(data []byte) [sha512.Size]byte {
+	hash := sha512.Sum512(data)
+
+	return hash
+}
+
+func HashFile(fname *string) ([sha512.Size]byte, error) {
+	data, err := ioutil.ReadFile(*fname)
+	if err != nil {
+		return [sha512.Size]byte{0}, err
+	}
+
+	return HashStr(data), nil
+}
