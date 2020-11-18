b696d72285ff3b76213471f2cb5b36596e31636c
r0ck3r008 <naman.arora@tutanota.com>
2020-11-17 19:10:55 -0500

    Shift to a goroutine reader and writer loop architecture
            The original node.SrvLoop is not broken up into
            Connector.ReadLoop as well as Connector.WriteLoop. This makes
            sure that the connector is duplex concurrently. Both the reader
            and writer loops have their own channels to send/receive data
            to/from.
            The WriteLoop sends out packets it received from send
            channel and the ReadLoop sends the received packets to
            receive channel to another goroutine, within Connecotr,
            Connector.Collector.
            The go routines Connector.{Collector, ReadLoop, WriteLoop} are
            initiated by the Node in NodeInit call. All the packets are
            transferred using the Envelop type on the channels.

--- connector/connector.go
+++ connector/connector.go
@@ -10,6 +10,12 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+type Envelope struct {
+	id   int64
+	cmds []byte
+	addr net.UDPAddr
+}
+
 type Connector struct {
 	conn   *net.UDPConn
 	pcache map[int64]Envelope
@@ -23,13 +29,58 @@ func ConnectorInit(addr *string) (*Connector, error) {
 	if err != nil {
 		return nil, fmt.Errorf("UDP Create: %s", err)
 	}
+	sch := make(chan Envelope, 100)
+	rch := make(chan Envelope, 100)
+	var mut *sync.Mutex = &sync.Mutex{}
 
+	var conn *Connector = &Connector{conn: conn_p, sch: sch, rch: rch, mut: mut}
 	return conn, nil
 }
 
+func (conn_p *Connector) Collector() {
+	for env := range conn_p.sch {
+		// Acquire write lock and write to cache
+		conn_p.mut.Lock()
+		conn_p.pcache[env.id] = env
+		conn_p.mut.Unlock()
 	}
+}
+func (conn_p *Connector) ReadLoop() {
+	for {
+		var cmdr []byte
+		_, addr_p, err := conn_p.conn.ReadFromUDP(cmdr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in reading: %s\n", err)
+			close(conn_p.rch)
+			break
+		}
+		// Extra UnMarshal due to pesky Mutex in Google Protobuf which stops from being sent on a channel
+		var pkt *Pkt = &Pkt{}
+		err = proto.Unmarshal(cmdr, pkt)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in unmarshalling: %s\n", err)
+			os.Exit(1)
+		}
+		hops := pkt.GetHops()
+		if hops != 0 {
+			pkt.Hops = hops - 1
+			var id int64 = pkt.GetRandNum()
+			cmdr, err = proto.Marshal(pkt)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in marshaling: %s\n", err)
+				os.Exit(1)
+			}
+			var env Envelope = Envelope{id, cmdr, *addr_p}
+			conn_p.sch <- env
+		}
 	}
 }
 
+func (conn_p *Connector) WriteLoop() {
+	for env := range conn_p.sch {
+		if _, err := conn_p.conn.WriteToUDP(env.cmds, &env.addr); err != nil {
+			fmt.Fprintf(os.Stderr, "Error in writing: %s\n", err)
+			break
+		}
 	}
 }
--- node/node.go
+++ node/node.go
@@ -9,15 +9,13 @@ package node
 import (
 	"fmt"
 	"math/rand"
-	"os"
 	"strconv"
-	"time"
+	"sync"
 
 	"github.com/r0ck3r008/kademgo/connector"
 	"github.com/r0ck3r008/kademgo/nbrmap"
 	"github.com/r0ck3r008/kademgo/objstore"
 	"github.com/r0ck3r008/kademgo/utils"
-	"google.golang.org/protobuf/proto"
 )
 
 // Node structure encapsulates the UDP listening port, objstore object,
@@ -27,6 +25,7 @@ type Node struct {
 	ost  *objstore.ObjStore
 	hash [utils.HASHSZ]byte
 	conn *connector.Connector
+	wg   *sync.WaitGroup
 }
 
 // NodeInit is the function that initiates the ObjStore, NbrMap, UDP listener
@@ -45,42 +44,25 @@ func NodeInit(addr *string) (*Node, error) {
 		return nil, fmt.Errorf("Error in starting listener: %s\n", err)
 	}
 	node_p.conn = conn
+	var wg = &sync.WaitGroup{}
+	wg.Add(3)
+	go func() {
+		conn.ReadLoop()
+		wg.Done()
+	}()
+	go func() {
+		conn.WriteLoop()
+		wg.Done()
+	}()
+	go func() {
+		conn.Collector()
+		wg.Done()
+	}()
+	node_p.wg = wg
 
 	return node_p, nil
 }
 
-// SrvLoop is the main loop that listens for messages and selects which go routine to launch
-// based on the type of the message received.
-func (node_p *Node) SrvLoop() {
-	for {
-		// Read incoming data from socket (Make sure this is the right length)
-		var cmdr []byte = make([]byte, 512)
-		_, addr_p, err := node_p.conn.ReadFromUDP(cmdr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error in reading: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Unmarshal the data
-		var pkt_p *Pkt = &Pkt{}
-		if err := proto.Unmarshal(cmdr, pkt_p); err != nil {
-			fmt.Fprintf(os.Stderr, "Error in unmarshalling: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Check for possible packet type
-		switch pkt_p.GetType() {
-		case Pkt_PingReq:
-			go node_p.PingRep(addr_p)
-		case Pkt_PingRep:
-		default:
-			fmt.Fprintf(os.Stderr, "Unknown type received!\n")
-			os.Exit(1)
-		}
-	}
-}
-
-// DeInit closes the listening UDP connection and makes the node exit gracefully.
 func (node_p *Node) DeInit() {
-	node_p.conn.Close()
+	node_p.wg.Wait()
 }
