d06e4452688b6f9779e8472b1e6bfe8d9a37e55e
r0ck3r008 <naman.arora@tutanota.com>
2020-11-17 19:08:55 -0500

    Move Ping to connector/api.go
            The api.go source file is supposed to handle all the API
            requests as methods of the Connector type. Also, PingReq/PingRes
            are now merged into one single request which returns a bool
            status representing if the host pinged is alive or not.

---
+++ connector/api.go
@@ -0,0 +1,38 @@
+package connector
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	"github.com/r0ck3r008/kademgo/utils"
+	"google.golang.org/protobuf/proto"
+)
+
+// PingReq requests for a ping reply from the passed in UDP address.
+func (conn_p *Connector) Ping(srchash *[utils.HASHSZ]byte, addr_p *net.UDPAddr) bool {
+	var rand_num int64 = int64(rand.Int())
+	addr := *addr_p
+	var pkt Pkt = Pkt{Type: Pkt_PingReq, Hash: hex.EncodeToString((*srchash)[:]), RandNum: rand_num, Hops: utils.MAXHOPS}
+	cmds, err := proto.Marshal(&pkt)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error in marshaling: %s\n", err)
+		os.Exit(1)
+	}
+	var env Envelope = Envelope{rand_num, cmds, addr}
+	conn_p.sch <- env
+	time.Sleep(utils.PINGWAIT)
+	// Fetch result from map
+	var ret bool = false
+	conn_p.mut.Lock()
+	if _, ok := conn_p.pcache[rand_num]; ok {
+		ret = true
+		delete(conn_p.pcache, rand_num)
+	}
+	conn_p.mut.Unlock()
+
+	return ret
+}
--- connector/connector.go
+++ connector/connector.go
@@ -1,19 +1,21 @@
 package connector
 
 import (
-	"encoding/hex"
 	"fmt"
 	"net"
 	"os"
+	sync "sync"
 
-	"github.com/r0ck3r008/kademgo/node"
 	"github.com/r0ck3r008/kademgo/utils"
 	"google.golang.org/protobuf/proto"
 )
 
 type Connector struct {
 	conn   *net.UDPConn
-	pinger *net.UDPConn
+	pcache map[int64]Envelope
+	mut    *sync.Mutex
+	sch    chan Envelope
+	rch    chan Envelope
 }
 
 func ConnectorInit(addr *string) (*Connector, error) {
@@ -22,39 +24,12 @@ func ConnectorInit(addr *string) (*Connector, error) {
 		return nil, fmt.Errorf("UDP Create: %s", err)
 	}
 
-	pinger_p, err := net.ListenUDP("conn", &net.UDPAddr{IP: []byte(*addr), Port: utils.PINGPORT, Zone: ""})
-	if err != nil {
-		return nil, fmt.Errorf("UDP Create: %s", err)
-	}
-
-	var conn *Connector = &Connector{conn: conn_p, pinger: pinger_p}
 	return conn, nil
 }
 
-// PingReq requests for a ping reply from the passed in UDP address.
-func (conn_p *Connector) PingReq(hash *[utils.HASHSZ]byte, addr_p *net.IP) {
-	var pkt_req *node.Pkt = &Pkt{Type: Pkt_PingReq, Hash: hex.EncodeToString((*hash)[:])}
-	cmds, err := proto.Marshal(pkt_req)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error in marshalling: %s\n", err)
-		os.Exit(1)
 	}
-	if _, err := conn_p.pinger.WriteToUDP(cmds, &net.UDPAddr{IP: *addr_p, Port: utils.PINGPORT, Zone: ""}); err != nil {
-		fmt.Fprintf(os.Stderr, "Error in sending UDP msg: %s\n", err)
-		os.Exit(1)
 	}
 }
 
-// PingRep replies a to a ping request to the neighbour passed in as parameter.
-func (conn_p *Connector) PingRep(hash *[utils.HASHSZ]byte, addr_p *net.IP) {
-	var pkt_rep *node.Pkt = &Pkt{Type: Pkt_PingRep, Hash: hex.EncodeToString((*hash)[:])}
-	cmds, err := proto.Marshal(pkt_rep)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error in marshalling: %s\n", err)
-		os.Exit(1)
-	}
-	if _, err := conn_p.pinger.WriteToUDP(cmds, &net.UDPAddr{IP: *addr_p, Port: utils.PINGPORT, Zone: ""}); err != nil {
-		fmt.Fprintf(os.Stderr, "Error in sending UDP msg: %s\n", err)
-		os.Exit(1)
 	}
 }
