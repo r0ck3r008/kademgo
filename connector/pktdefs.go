d4a2b71a8d37b62a722b5ee3c8cf5e0be029138e
r0ck3r008 <naman.arora@tutanota.com>
2020-11-19 03:35:59 -0500

    Moving away from protobuf to json

---
+++ connector/pktdefs.go
@@ -0,0 +1,39 @@
+package connector
+
+import "github.com/r0ck3r008/kademgo/utils"
+
+// PktType is the type that defines what the message/packet
+// is supposed to represent.
+type PktType int
+
+const (
+	// BeginReq is the message that is sent when a node first
+	// comes online
+	BeginReq PktType = iota
+
+	// BeginRes is the message that is the reply to BeginReq. Any
+	// node is capable of sedning it.
+	BeginRes
+
+	// PingReq is the is-alive request.
+	PingReq
+
+	// PingRes is the response to is-alive request.
+	PingRes
+)
+
+type Pkt struct {
+	// Ttl is the number of hops of this particular message.
+	// The message should be dropped if it ever is 0.
+	Ttl int
+
+	// RandNum is a long long int identifier of this message.
+	// This facilitates easy caching of packets internally.
+	RandNum int64
+
+	// Hash is the hash of the node sending the message.
+	Hash [utils.HASHSZ]byte
+
+	// Type is the type of packet from PktType.
+	Type PktType
+}
