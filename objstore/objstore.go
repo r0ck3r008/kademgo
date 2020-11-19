586cc076b2f9be6952ea9a6d25823d165fc36a47
r0ck3r008 <naman.arora@tutanota.com>
2020-11-19 17:01:11 -0500

    Redesigned ObjMap
            This ObjMap definition is a mapping of ObjNodes on the bases of
            distances from utils.GetDist. Each ObjNode contains a mapping of
            hash of the object to its index value in a vector within the
            same ObjNode. This facilitates easier extraction of object
            node/nodes.

--- objstore/objstore.go
+++ objstore/objstore.go
@@ -1,99 +1,57 @@
-// Objstore package is responsible for maintaing a prefix tree for the objects
-// that the current node possesses. Its provides insertion and checking for existence
-package objstore
+package objmap
 
-// ObjNode is the node that represents the Key, Value pair.
-// If object is empty slice, it means that it is not a leaf node.
+import (
+	"fmt"
+
+	"github.com/r0ck3r008/kademgo/utils"
+)
+
 type ObjNode struct {
-	hash []byte
-	cmap map[byte]*ObjNode
-	obj  []byte
+	nmap map[[utils.HASHSZ]byte]int
+	nvec []interface{}
 }
 
-// ObjStore is an abstraction of the root node of the prefix tree.
-type ObjStore struct {
-	root *ObjNode
+type ObjMap struct {
+	omap map[int]*ObjNode
 }
 
-// getlvl finds the max character length upto where two hashes are the same.
-func getlvl(hash1 []byte, hash2 []byte) int {
-	var lvl int = 0
-	var mlen int
-	if len(hash1) < len(hash2) {
-		mlen = len(hash1)
-	} else {
-		mlen = len(hash2)
-	}
-	for lvl < mlen {
-		if hash1[lvl] != hash2[lvl] {
-			break
+func (omap_p *ObjMap) Init() {
+	omap_p.omap = make(map[int]*ObjNode)
+}
+
+func (omap_p *ObjMap) Insert(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte, obj interface{}) {
+	var indx int = utils.GetDist(srchash, dsthash)
+	if node_p, ok := omap_p.omap[indx]; ok {
+		if _, ok := node_p.nmap[*dsthash]; !ok {
+			omap_p.omap[indx].nmap[*dsthash] = len(omap_p.omap[indx].nvec)
+			omap_p.omap[indx].nvec = append(omap_p.omap[indx].nvec, obj)
 		}
-		lvl++
+	} else {
+		var node_p *ObjNode = &ObjNode{make(map[[utils.HASHSZ]byte]int), []interface{}{obj}}
+		node_p.nmap[*dsthash] = 0
+		omap_p.omap[indx] = node_p
 	}
-
-	return lvl
 }
 
-// hash_update function updates the hash map of a particular ObjNode.
-func hash_update(obn_p *ObjNode, slice []byte, indx byte, obj []byte, lvl int) {
-	if node_p, ok := obn_p.cmap[indx]; ok {
-		node_p.insert(slice, obj)
-	} else {
-		obn_p.cmap[indx] = &ObjNode{
-			slice,
-			make(map[byte]*ObjNode),
-			obj,
+func (omap_p *ObjMap) Get(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) (*interface{}, error) {
+	var indx int = utils.GetDist(srchash, dsthash)
+	if node_p, ok := omap_p.omap[indx]; ok {
+		if k, ok := node_p.nmap[*dsthash]; ok {
+			return &omap_p.omap[indx].nvec[k], nil
 		}
 	}
+
+	return nil, fmt.Errorf("Not Found!")
 }
 
-// insert recursively inserts an object according to its hash into the prefix tree.
-func (obn_p *ObjNode) insert(hash []byte, obj []byte) {
-	var lvl int = getlvl(obn_p.hash, hash)
-	if lvl > 0 {
-		var slice1 []byte = hash[lvl:]
-		var slice2 []byte = obn_p.hash[lvl:]
-		hash_update(obn_p, slice1, hash[lvl], obj, lvl)
-		hash_update(obn_p, slice2, obn_p.hash[lvl], obn_p.obj, lvl)
-		obn_p.hash = obn_p.hash[:lvl]
-		obn_p.obj = []byte{}
-	} else {
-		hash_update(obn_p, hash, hash[lvl], obj, 0)
-	}
-}
-
-// find recursively checks if the given hash is present in the prefix tree.
-func (obn_p *ObjNode) find(hash []byte) bool {
-	var lvl int = getlvl(obn_p.hash, hash)
-	if lvl == len(hash) && len(obn_p.obj) != 0 {
-		if lvl == len(obn_p.hash) {
-			return true
-		} else {
-			return false
+func (omap_p *ObjMap) GetAll(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) []*[]interface{} {
+	var indx int = utils.GetDist(srchash, dsthash)
+	var ret []*[]interface{}
+	for i := 1; i <= indx; i++ {
+		if node_p, ok := omap_p.omap[i]; ok {
+			ret = append(ret, &node_p.nvec)
 		}
-	} else if node_p, ok := obn_p.cmap[hash[lvl]]; ok {
-		return node_p.find(hash[lvl:])
 	}
 
-	return false
-}
-
-// Init initiates the root in ObjStore.
-func (ost_p *ObjStore) Init() {
-	var obj []byte
-	ost_p.root = &ObjNode{
-		[]byte{0},
-		make(map[byte]*ObjNode),
-		obj,
-	}
-}
-
-// Insert is an encapsulation over ObjNode.insert.
-func (ost_p *ObjStore) Insert(hash []byte, obj []byte) {
-	ost_p.root.insert(hash, obj)
-}
-
-// Find is an encapsulation over ObjNode.find.
-func (ost_p *ObjStore) Find(hash []byte) bool {
-	return ost_p.root.find(hash)
+	return ret
 }
