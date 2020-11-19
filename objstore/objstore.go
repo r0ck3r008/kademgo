8328c3edd2f9ad9a97f99ce711576bcb54333f5a
r0ck3r008 <naman.arora@tutanota.com>
2020-11-19 00:36:54 -0500

    Obj of the ObjNode is a slice which will have net.IP

--- objstore/objstore.go
+++ objstore/objstore.go
@@ -35,7 +35,7 @@ func getlvl(hash1 []byte, hash2 []byte) int {
 }
 
 // hash_update function updates the hash map of a particular ObjNode.
-func hash_update(obn_p *ObjNode, slice []byte, indx byte, obj interface{}, lvl int) {
+func hash_update(obn_p *ObjNode, slice []byte, indx byte, obj []byte, lvl int) {
 	if node_p, ok := obn_p.cmap[indx]; ok {
 		node_p.insert(slice, obj)
 	} else {
@@ -48,49 +48,48 @@ func hash_update(obn_p *ObjNode, slice []byte, indx byte, obj interface{}, lvl i
 }
 
 // insert recursively inserts an object according to its hash into the prefix tree.
-func (obn_p *ObjNode) insert(hash []byte, obj interface{}) {
+func (obn_p *ObjNode) insert(hash []byte, obj []byte) {
 	var lvl int = getlvl(obn_p.hash, hash)
-	if lvl >= 0 {
+	if lvl > 0 {
 		var slice1 []byte = hash[lvl:]
 		var slice2 []byte = obn_p.hash[lvl:]
-		var indx byte = hash[lvl]
-		hash_update(obn_p, slice1, indx, obj, lvl)
-		hash_update(obn_p, slice2, indx, obn_p.obj, lvl)
+		hash_update(obn_p, slice1, hash[lvl], obj, lvl)
+		hash_update(obn_p, slice2, obn_p.hash[lvl], obn_p.obj, lvl)
 		obn_p.hash = obn_p.hash[:lvl]
-		obn_p.obj = nil
+		obn_p.obj = []byte{}
+	} else {
+		hash_update(obn_p, hash, hash[lvl], obj, 0)
 	}
 }
 
 // find recursively checks if the given hash is present in the prefix tree.
 func (obn_p *ObjNode) find(hash []byte) bool {
 	var lvl int = getlvl(obn_p.hash, hash)
-	if lvl == utils.KVAL-1 {
-		return true
-	} else if lvl >= 0 {
-		var indx byte = hash[lvl]
-		if node_p, ok := obn_p.cmap[indx]; ok {
-			var slice []byte = hash[lvl:]
-			return node_p.find(slice)
+	if lvl == len(hash) && len(obn_p.obj) != 0 {
+		if lvl == len(obn_p.hash) {
+			return true
+		} else {
+			return false
 		}
+	} else if node_p, ok := obn_p.cmap[hash[lvl]]; ok {
+		return node_p.find(hash[lvl:])
 	}
 
 	return false
 }
 
 // ObjStoreInit initiates the root in ObjStore.
-func ObjStoreInit() (ost_p *ObjStore) {
-	ost_p = &ObjStore{}
+func (ost_p *ObjStore) Init() {
+	var obj []byte
 	ost_p.root = &ObjNode{
 		[]byte{0},
 		make(map[byte]*ObjNode),
-		nil,
+		obj,
 	}
-
-	return ost_p
 }
 
 // Insert is an encapsulation over ObjNode.insert.
-func (ost_p *ObjStore) Insert(hash []byte, obj interface{}) {
+func (ost_p *ObjStore) Insert(hash []byte, obj []byte) {
 	ost_p.root.insert(hash, obj)
 }
 
