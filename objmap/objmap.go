74538237dbd28b4687b1d5be4ddf208f9f199170
r0ck3r008 <naman.arora@tutanota.com>
2020-11-20 17:53:56 -0500

    Just net.IP is enough to send as object value

--- objmap/objmap.go
+++ objmap/objmap.go
@@ -16,7 +16,7 @@ import (
 // Each object has its index mapped using its hash within the same node.
 type ObjNode struct {
 	nmap map[[utils.HASHSZ]byte]int
-	nvec []net.UDPAddr
+	nvec []net.IP
 }
 
 // ObjMap is the high level mapping of distances from the node to seprate buckets.
@@ -30,7 +30,7 @@ func (omap_p *ObjMap) Init() {
 }
 
 // Insert inserts the object accoring to its distance from the node.
-func (omap_p *ObjMap) Insert(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte, obj *net.UDPAddr) {
+func (omap_p *ObjMap) Insert(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte, obj *net.IP) {
 	var indx int = utils.GetDist(srchash, dsthash)
 	if node_p, ok := omap_p.omap[indx]; ok {
 		if _, ok := node_p.nmap[*dsthash]; !ok {
@@ -38,14 +38,14 @@ func (omap_p *ObjMap) Insert(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ
 			omap_p.omap[indx].nvec = append(omap_p.omap[indx].nvec, *obj)
 		}
 	} else {
-		var node_p *ObjNode = &ObjNode{make(map[[utils.HASHSZ]byte]int), []net.UDPAddr{*obj}}
+		var node_p *ObjNode = &ObjNode{make(map[[utils.HASHSZ]byte]int), []net.IP{*obj}}
 		node_p.nmap[*dsthash] = 0
 		omap_p.omap[indx] = node_p
 	}
 }
 
 // Get fetches the object if it exists.
-func (omap_p *ObjMap) Get(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) (*net.UDPAddr, error) {
+func (omap_p *ObjMap) Get(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) (*net.IP, error) {
 	var indx int = utils.GetDist(srchash, dsthash)
 	if node_p, ok := omap_p.omap[indx]; ok {
 		if k, ok := node_p.nmap[*dsthash]; ok {
@@ -58,9 +58,9 @@ func (omap_p *ObjMap) Get(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]by
 
 // GetAll returns all the objects that are closer to the given hash from the src hash as a list of
 // pointers to a slice of objects.
-func (omap_p *ObjMap) GetAll(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) []*[]net.UDPAddr {
+func (omap_p *ObjMap) GetAll(srchash *[utils.HASHSZ]byte, dsthash *[utils.HASHSZ]byte) []*[]net.IP {
 	var indx int = utils.GetDist(srchash, dsthash)
-	var ret []*[]net.UDPAddr
+	var ret []*[]net.IP
 	for i := 1; i <= indx; i++ {
 		if node_p, ok := omap_p.omap[i]; ok {
 			ret = append(ret, &node_p.nvec)
