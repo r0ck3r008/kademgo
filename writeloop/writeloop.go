e34559e846b94d4f6ba4c58065c1a369cec43f41
r0ck3r008 <naman.arora@tutanota.com>
2020-11-22 00:28:36 -0500

    Updated definitions in compliance with 0b038d8

--- writeloop/writeloop.go
+++ writeloop/writeloop.go
@@ -10,18 +10,19 @@ import (
 	"time"
 
 	"github.com/r0ck3r008/kademgo/objmap"
+	"github.com/r0ck3r008/kademgo/pkt"
 	"github.com/r0ck3r008/kademgo/utils"
 )
 
 type WriteLoop struct {
-	pcache *map[int64]utils.Envelope
+	pcache *map[int64]pkt.Envelope
 	mut    *sync.Mutex
 	// sch has only ony sink and a lot of sources.
 	// The sinc is WriteLoop
-	sch chan utils.Envelope
+	sch chan pkt.Envelope
 }
 
-func (wrl_p *WriteLoop) Init(mut *sync.Mutex, pcache *map[int64]utils.Envelope, sch chan utils.Envelope) {
+func (wrl_p *WriteLoop) Init(mut *sync.Mutex, pcache *map[int64]pkt.Envelope, sch chan pkt.Envelope) {
 	wrl_p.mut = mut
 	wrl_p.pcache = pcache
 	wrl_p.sch = sch
@@ -31,12 +32,12 @@ func (wrl_p *WriteLoop) Init(mut *sync.Mutex, pcache *map[int64]utils.Envelope, 
 // from the node and send them asynchronously to the desired destinations.
 func (wrl_p *WriteLoop) WriteLoop(conn_p *net.UDPConn) {
 	for env := range wrl_p.sch {
-		cmds, err := json.Marshal(env.pkt)
+		cmds, err := json.Marshal(env.Pkt)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error in Marshalling: %s\n", err)
 			break
 		}
-		if _, err := conn_p.WriteToUDP(cmds, &env.addr); err != nil {
+		if _, err := conn_p.WriteToUDP(cmds, &env.Addr); err != nil {
 			fmt.Fprintf(os.Stderr, "Error in writing: %s\n", err)
 			break
 		}
@@ -48,16 +49,16 @@ func (wrl_p *WriteLoop) WriteLoop(conn_p *net.UDPConn) {
 func (wrl_p *WriteLoop) Ping(srchash *[utils.HASHSZ]byte, addr_p *net.UDPAddr) bool {
 	var rand_num int64 = int64(rand.Int())
 	addr := *addr_p
-	var pkt utils.Pkt = utils.Pkt{Ttl: utils.MAXHOPS, RandNum: rand_num, Hash: *srchash, Type: PingReq}
-	var env utils.Envelope = utils.Envelope{pkt, addr}
+	var packet pkt.Packet = pkt.Packet{Ttl: utils.MAXHOPS, RandNum: rand_num, Hash: *srchash, Type: pkt.PingReq}
+	var env pkt.Envelope = pkt.Envelope{Pkt: packet, Addr: addr}
 	wrl_p.sch <- env
 	time.Sleep(utils.PINGWAIT)
 	// Fetch result from map
 	var ret bool = false
 	wrl_p.mut.Lock()
-	if _, ok := wrl_p.pcache[rand_num]; ok {
+	if _, ok := (*wrl_p.pcache)[rand_num]; ok {
 		ret = true
-		delete(wrl_p.pcache, rand_num)
+		delete((*wrl_p.pcache), rand_num)
 	}
 	wrl_p.mut.Unlock()
 
@@ -68,8 +69,8 @@ func (wrl_p *WriteLoop) Ping(srchash *[utils.HASHSZ]byte, addr_p *net.UDPAddr) b
 func (wrl_p *WriteLoop) Store(srchash *[utils.HASHSZ]byte, addr_p *net.UDPAddr, obj_p *objmap.ObjNode) {
 	var rand_num int64 = int64(rand.Int())
 	obj := *obj_p
-	var pkt utils.Pkt = utils.Pkt{Ttl: utils.MAXHOPS, RandNum: rand_num, Type: Store, Hash: *srchash, Obj: obj}
-	var env utils.Envelope = utils.Envelope{pkt, *addr_p}
+	var packet pkt.Packet = pkt.Packet{Ttl: utils.MAXHOPS, RandNum: rand_num, Type: pkt.Store, Hash: *srchash, Obj: obj}
+	var env pkt.Envelope = pkt.Envelope{Pkt: packet, Addr: *addr_p}
 
 	wrl_p.sch <- env
 }
