73c00ed428dcb5d78772de93c06a80b52e6c4bc6
r0ck3r008 <naman.arora@tutanota.com>
2020-11-22 00:27:59 -0500

    Update definitions in compliance with 0b038d8

--- readloop/readloop.go
+++ readloop/readloop.go
@@ -9,20 +9,20 @@ import (
 
 	"github.com/r0ck3r008/kademgo/nbrmap"
 	"github.com/r0ck3r008/kademgo/objmap"
-	"github.com/r0ck3r008/kademgo/utils"
+	"github.com/r0ck3r008/kademgo/pkt"
 )
 
 type ReadLoop struct {
-	rch    chan utils.Envelope
-	sch    chan<- utils.Envelope
-	pcache *map[int64]utils.Envelope
+	rch    chan pkt.Envelope
+	sch    chan<- pkt.Envelope
+	pcache *map[int64]pkt.Envelope
 	mut    *sync.Mutex
 	nmap   *nbrmap.NbrMap
 	ost    *objmap.ObjMap
 }
 
-func (rdl_p *ReadLoop) Init(mut *sync.Mutex, pcache *map[int64]utils.Envelope, sch chan<- utils.Envelope) {
-	rdl_p.rch = make(chan Envelope, 100)
+func (rdl_p *ReadLoop) Init(mut *sync.Mutex, pcache *map[int64]pkt.Envelope, sch chan<- pkt.Envelope) {
+	rdl_p.rch = make(chan pkt.Envelope, 100)
 	rdl_p.sch = sch
 	rdl_p.mut = mut
 	rdl_p.pcache = pcache
@@ -42,17 +42,17 @@ func (rdl_p *ReadLoop) DeInit() {
 func (rdl_p *ReadLoop) Collector() {
 	wg := sync.WaitGroup{}
 	for env := range rdl_p.rch {
-		switch env.pkt.Type {
-		case PingReq:
+		switch env.Pkt.Type {
+		case pkt.PingReq:
 			wg.Add(1)
 			go func() { rdl_p.PingRes(env); wg.Done() }()
-		case Store:
+		case pkt.Store:
 			wg.Add(1)
 			go func() { rdl_p.StoreHandler(env); wg.Done() }()
 		default:
 			// Acquire write lock and write to cache
 			rdl_p.mut.Lock()
-			rdl_p.pcache[env.pkt.RandNum] = env
+			(*rdl_p.pcache)[env.Pkt.RandNum] = env
 			rdl_p.mut.Unlock()
 		}
 	}
@@ -69,27 +69,27 @@ func (rdl_p *ReadLoop) ReadLoop(conn_p *net.UDPConn) {
 			fmt.Fprintf(os.Stderr, "Error in reading: %s\n", err)
 			break
 		}
-		var pkt Pkt = Pkt{}
-		err = json.Unmarshal(cmdr, &pkt)
+		var packet pkt.Packet = pkt.Packet{}
+		err = json.Unmarshal(cmdr, &packet)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error in unmarshalling: %s\n", err)
 			os.Exit(1)
 		}
-		if pkt.Ttl != 0 {
-			pkt.Ttl--
-			var env utils.Envelope = utils.Envelope{pkt, *addr_p}
+		if packet.Ttl != 0 {
+			packet.Ttl--
+			var env pkt.Envelope = pkt.Envelope{Pkt: packet, Addr: *addr_p}
 			rdl_p.rch <- env
 		}
 	}
 }
 
-func (rdl_p *ReadLoop) PingRes(env utils.Envelope) {
-	env.pkt.Type = PingRes
+func (rdl_p *ReadLoop) PingRes(env pkt.Envelope) {
+	env.Pkt.Type = pkt.PingRes
 	rdl_p.sch <- env
 
 	// Insert to the NbrMap here.
 }
 
-func (rdl_p *ReadLoop) StoreHandler(env utils.Envelope) {
+func (rdl_p *ReadLoop) StoreHandler(env pkt.Envelope) {
 	// Insert to the ObjMap here
 }
