fd8c03f87770e06fbcbf8b8c206be97dd88a652a
r0ck3r008 <naman.arora@tutanota.com>
2020-11-22 00:30:26 -0500

    Refer commits 0b038d8, 73c00ed and e34559e

--- node/node.go
+++ node/node.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 	"sync"
 
+	"github.com/r0ck3r008/kademgo/pkt"
 	"github.com/r0ck3r008/kademgo/readloop"
 	"github.com/r0ck3r008/kademgo/utils"
 	"github.com/r0ck3r008/kademgo/writeloop"
@@ -21,8 +22,8 @@ import (
 // NbrMap object as well as the hash of the node in question.
 type Node struct {
 	hash   [utils.HASHSZ]byte
-	sch    chan utils.Envelope
-	pcache map[int64]utils.Envelope
+	sch    chan pkt.Envelope
+	pcache map[int64]pkt.Envelope
 	conn   *net.UDPConn
 	wg     *sync.WaitGroup
 	rdl    *readloop.ReadLoop
@@ -33,17 +34,17 @@ type Node struct {
 // Init is the function that initiates the ObjStore, NbrMap, UDP listener
 // and as well as forms the random hash for the node.
 func (node_p *Node) Init(addr *string, gway_addr *net.UDPAddr) error {
-	node_p.pcache = make(map[int64]utils.Envelope)
+	node_p.pcache = make(map[int64]pkt.Envelope)
 	node_p.mut = &sync.Mutex{}
 	node_p.wg = &sync.WaitGroup{}
 	var rnum_str string = strconv.FormatInt(int64(rand.Int()), 10)
 	node_p.hash = utils.HashStr([]byte(rnum_str))
-	node_p.sch = make(chan utils.Envelope, 100)
+	node_p.sch = make(chan pkt.Envelope, 100)
 
 	node_p.rdl = &readloop.ReadLoop{}
 	node_p.wrl = &writeloop.WriteLoop{}
-	node_p.rdl.Init(rdl_p.mut, &node_p.pcache, node_p.sch)
-	node_p.wrl.Init(rdl_p.mut, &node_p.pcache, node_p.sch)
+	node_p.rdl.Init(node_p.mut, &node_p.pcache, node_p.sch)
+	node_p.wrl.Init(node_p.mut, &node_p.pcache, node_p.sch)
 
 	var err error
 	node_p.conn, err = net.ListenUDP("conn", &net.UDPAddr{IP: []byte(*addr), Port: utils.PORTNUM, Zone: ""})
