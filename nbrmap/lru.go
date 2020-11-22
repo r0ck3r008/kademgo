5278512897ef900b9353fff4fe7d476386deb162
r0ck3r008 <naman.arora@tutanota.com>
2020-11-22 13:55:28 -0500

    access struct can be replaced by pkt.ObjAddr

--- nbrmap/lru.go
+++ nbrmap/lru.go
@@ -2,23 +2,18 @@ package nbrmap
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/r0ck3r008/kademgo/pkt"
 	"github.com/r0ck3r008/kademgo/utils"
 	"github.com/r0ck3r008/kademgo/writeloop"
 )
 
-// access maps the address of a neighbor back to its saved hash.
-type access struct {
-	obj  *pkt.ObjAddr
-	hash [utils.HASHSZ]byte
-}
-
 // NbrNode serves as the LRU cache for the Neighbours and represents a particular
 // K-Bucket within the larger NeighbourMap
 type NbrNode struct {
 	cmap map[[utils.HASHSZ]byte]int
-	cvec []*access
+	cvec []*pkt.ObjAddr
 	sz   int
 }
 
@@ -26,7 +21,7 @@ type NbrNode struct {
 func nbrnodeinit() (cache_p *NbrNode) {
 	cache_p = &NbrNode{}
 	cache_p.cmap = make(map[[utils.HASHSZ]byte]int)
-	cache_p.cvec = make([]*access, utils.KVAL)
+	cache_p.cvec = make([]*pkt.ObjAddr, utils.KVAL)
 	cache_p.sz = 0
 
 	return cache_p
@@ -35,12 +30,11 @@ func nbrnodeinit() (cache_p *NbrNode) {
 // put is used to insert a new neighbour into the cached list of neighbours.
 // It uses LRU eviction policy based on an irresponsive last contacted neighbour
 // in case of a filled bucket.
-func (cache_p *NbrNode) put(srchash, dsthash *[utils.HASHSZ]byte, obj *pkt.ObjAddr, wrl_p *writeloop.WriteLoop) {
+func (cache_p *NbrNode) put(srchash, dsthash *[utils.HASHSZ]byte, obj *net.IP, wrl_p *writeloop.WriteLoop) {
 	if indx, ok := cache_p.cmap[*dsthash]; ok && (indx != len(cache_p.cvec)-1) {
 		// Found! Now remove it from where ever it is and push to the back
 		cache_p.cvec = append(cache_p.cvec[:indx], cache_p.cvec[indx+1:]...)
-		var addr pkt.ObjAddr = *obj
-		cache_p.cvec = append(cache_p.cvec, &access{&addr, *dsthash})
+		cache_p.cvec = append(cache_p.cvec, &pkt.ObjAddr{Hash: *dsthash, Addr: *obj})
 	} else {
 		// Not Found
 		// if length of cvec is == KVAL, remove the first element from front
@@ -48,14 +42,13 @@ func (cache_p *NbrNode) put(srchash, dsthash *[utils.HASHSZ]byte, obj *pkt.ObjAd
 		veclen := len(cache_p.cvec)
 		if veclen == utils.KVAL {
 			// Remove the lease recently used
-			var old_p *access
+			var old_p *pkt.ObjAddr
 			old_p, cache_p.cvec = cache_p.cvec[0], cache_p.cvec[1:]
 
-			if !wrl_p.Ping(srchash, &old_p.obj.Addr) {
+			if !wrl_p.Ping(srchash, &old_p.Addr) {
 				// If ping fails, add the new one
-				delete(cache_p.cmap, old_p.hash)
-				var addr pkt.ObjAddr = *obj
-				cache_p.cvec = append(cache_p.cvec, &access{&addr, *dsthash})
+				delete(cache_p.cmap, old_p.Hash)
+				cache_p.cvec = append(cache_p.cvec, &pkt.ObjAddr{Hash: *dsthash, Addr: *obj})
 				cache_p.cmap[*dsthash] = veclen
 			} else {
 				// If ping succeedes, add the old one to back
@@ -68,7 +61,7 @@ func (cache_p *NbrNode) put(srchash, dsthash *[utils.HASHSZ]byte, obj *pkt.ObjAd
 // get fetches the neighbour if it exists in the cache, returns error on faliure.
 func (cache_p *NbrNode) get(hash *[utils.HASHSZ]byte) (*pkt.ObjAddr, error) {
 	if indx, ok := cache_p.cmap[*hash]; ok {
-		return cache_p.cvec[indx].obj, nil
+		return cache_p.cvec[indx], nil
 	}
 
 	return nil, fmt.Errorf("Not Found!")
