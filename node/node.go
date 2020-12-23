a5f6edb8ad98f0a54b22883cb1efe3fec416de62
r0ck3r008 <naman.arora@tutanota.com>
2020-12-23 00:26:29 -0500

    Node.collector function to process pkts from Connector

--- node/node.go
+++ node/node.go
@@ -8,20 +8,25 @@ package node
 import (
 	"math/rand"
 	"strconv"
+	"sync"
 
 	"github.com/r0ck3r008/kademgo/connector"
 	"github.com/r0ck3r008/kademgo/nbrmap"
 	"github.com/r0ck3r008/kademgo/objmap"
+	"github.com/r0ck3r008/kademgo/pkt"
 	"github.com/r0ck3r008/kademgo/utils"
 )
 
 // Node structure encapsulates the UDP listening port, objstore object,
 // NbrMap object as well as the hash of the node in question.
 type Node struct {
-	hash   [utils.HASHSZ]byte
-	conn   *connector.Connector
-	omap   *objmap.ObjMap
-	nmap   *nbrmap.NbrMap
+	// hash is the SHA512 hash ID of the node.
+	hash  [utils.HASHSZ]byte
+	nchan chan pkt.Envelope
+	conn  *connector.Connector
+	omap  *objmap.ObjMap
+	nmap  *nbrmap.NbrMap
+	wg    *sync.WaitGroup
 }
 
 // Init is the function that initiates the ReaderLoop, WriterLoop, UDP listener
@@ -37,11 +42,34 @@ func (node_p *Node) Init(addr *string, gway_addr *string) error {
 	node_p.nmap.Init()
 
 	node_p.conn = &connector.Connector{}
-	if err := node_p.conn.Init(addr); err != nil {
+	if err := node_p.conn.Init(addr, node_p.nchan); err != nil {
 		return err
 	}
 
+	node_p.nchan = make(chan pkt.Envelope)
+	node_p.wg = &sync.WaitGroup{}
+
+	node_p.wg.Add(1)
+	go func() { node_p.collector(); node_p.wg.Done() }()
+
 	return nil
+}
+
+// collector in Node collects all the packets from the readloop which require the
+// involvement of NbrMap or ObjMap and appropriately processes them.
+func (node_p *Node) collector() {
+	wg := sync.WaitGroup{}
+	for env := range node_p.nchan {
+		switch env.Pkt.Type {
+		case pkt.PingRes:
+			wg.Add(1)
+			go func() { node_p.nmap.Insert(node_p.hash, env.Pkt.Hash, env.Addr.IP, node_p.conn); wg.Done() }()
+		case pkt.Store:
+			wg.Add(1)
+			go func() { node_p.omap.Insert(node_p.hash, env.Pkt.Obj); wg.Done() }()
+		}
+	}
+	wg.Wait()
 }
 
 // DeInit function waits for all the go routines registered to exit.
