15633ef8cdb1c87a515792e1502d40453ff502cf
r0ck3r008 <naman.arora@tutanota.com>
2020-12-30 14:09:40 -0500

    Readloop has a read timeout and gets cue when to end
        endchan is used to cue readloop when to exit which when closed by
        the DeInit function call in Connector, breaks the infinite for loop.
        The timeout is set using the SetReadDeadline method of UDPConn.

--- connector/readloop.go
+++ connector/readloop.go
@@ -2,10 +2,13 @@ package connector
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/r0ck3r008/kademgo/pkt"
+	"github.com/r0ck3r008/kademgo/utils"
 )
 
 // Collector is intended to be a goroutine that process the received packets in form of Envelope
@@ -30,21 +33,36 @@ func (conn_p *Connector) collector() {
 // to the node and send those along, if the TTL has not expired, to the Collector.
 func (conn_p *Connector) readloop() {
 	for {
-		var cmdr []byte
-		_, addr_p, err := conn_p.conn.ReadFromUDP(cmdr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error in reading: %s\n", err)
+		select {
+		case <-conn_p.endchan:
 			break
+		default:
+			// Set timeout on read
+			if err := conn_p.conn.SetReadDeadline(time.Now().Add(10 * utils.TIMEOUT)); err != nil {
+				fmt.Fprintf(os.Stderr, "Error in setting the read timeout: %s\n", err)
+			}
+
+			// Allocate buffer to read into and read
+			var cmdr []byte
+			_, addr_p, err := conn_p.conn.ReadFromUDP(cmdr)
+			if err != nil {
+				if errors.Is(err, os.ErrDeadlineExceeded) {
+					continue
+				}
+				fmt.Fprintf(os.Stderr, "Error in reading: %s\n", err)
+				break
+			}
+
+			// Unmarshall into packet
+			var packet pkt.Packet = pkt.Packet{}
+			err = json.Unmarshal(cmdr, &packet)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in unmarshalling: %s\n", err)
+				os.Exit(1)
+			}
+			var env pkt.Envelope = pkt.Envelope{Pkt: packet, Addr: *addr_p}
+			// Send out for being handeled
+			conn_p.rch <- env
 		}
-		var packet pkt.Packet = pkt.Packet{}
-		err = json.Unmarshal(cmdr, &packet)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error in unmarshalling: %s\n", err)
-			os.Exit(1)
-		}
-		var env pkt.Envelope = pkt.Envelope{Pkt: packet, Addr: *addr_p}
-		// BUG: This might make the application panic if DeInit on Connector is called while
-		// receive channel is being written to with a new packet.
-		conn_p.rch <- env
 	}
 }
