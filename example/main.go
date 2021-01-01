ae48b3e4a828da70fa07d99eca2aad8f53478d2e
r0ck3r008 <naman.arora@tutanota.com>
2021-01-01 17:31:43 -0500

    Bug: Readloop exists prematurely
        The Readloop exists prematurely since DeInit call to Connector sends
        a true message to conn_p.endchan before endloop can select on
        endchan for the first time.
        Adding a timeout between Init and DeInit calls seems to prevent this
        behavior.

--- example/main.go
+++ example/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/r0ck3r008/kademgo"
 )
 
 func main() {
 	var node *kademgo.KademGo = &kademgo.KademGo{}
 	node.Init(nil, nil)
+
+	// Give time for Readloop to begin before sending deInit
+	time.Sleep(time.Second)
 	node.DeInit()
 }
